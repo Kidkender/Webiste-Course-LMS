e62bc467631acb47273433a4bb7b69ab78e83ba1
DuckNH <84608369+Kidkender@users.noreply.github.com>
2023-12-12 22:08:00 +0700

    add database prisma

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,10 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/contact", "/search"],
+  publicRoutes: ["/", "/contact", "/search", "/teacher/"],
 });
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  ignoredRoutes: ["/((?!api|trpc))(_next.*|.+.[w]+$)", "/teacher/courses"],
 };
